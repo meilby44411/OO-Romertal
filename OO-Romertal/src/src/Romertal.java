a4ead36feeded5f69f83e0d7a9d2cb62e6875bee
Lasse <Lasse@192.168.0.100>
2015-04-20 20:52:44 +0200

    1st test implementeret - fake it

---
+++ OO-Romertal/src/src/Romertal.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Romertal {
+	
+	public String calculate(String s1, String s2){
+		return "III";
+	}
+
+}
--- OO-Romertal/test/test/TestCases.java
+++ OO-Romertal/test/test/TestCases.java
@@ -5,14 +5,19 @@ import static org.junit.Assert.*;
 import org.junit.AfterClass;
 import org.junit.Test;
 
+import src.Romertal;
+
 public class TestCases {
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
 
+	Romertal romertal = new Romertal();
+	
 	@Test
 	public void testIplusIIequalsIII() {
+		assertEquals(romertal.calculate("I", "II"), "III");
 	}
 
 }
