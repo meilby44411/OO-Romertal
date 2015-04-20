d610649b2a850edd0bb38353e81e820780f250ff
Lasse <Lasse@192.168.0.100>
2015-04-20 21:15:30 +0200

    test implementeret

--- OO-Romertal/src/src/Romertal.java
+++ OO-Romertal/src/src/Romertal.java
@@ -2,36 +2,47 @@ package src;
 
 public class Romertal {
 	char[] a1 = new char[100];
-	
-	public String calculate(String s1, String s2){
+
+	public String calculate(String s1, String s2) {
 		String temp = s1 + s2;
 		a1 = temp.toCharArray();
 		String nyString = "";
-		
+
 		int antal_I = 0;
 		int antal_V = 0;
-		
-		for(char c : a1){
-			if(c == 'I')
+		int antal_X = 0;
+
+		for (char c : a1) {
+			if (c == 'I')
 				antal_I++;
-		}
-		
-		while (antal_I >=5){
-			antal_V++;
-			antal_I -=5;
-		}
+			else if(c == 'V')
+				antal_V++;
 			
-		for	(int i = 0; i < antal_V; i++) {
+		}
+
+		while (antal_I >= 5) {
+			antal_V++;
+			antal_I -= 5;
+		}
+
+		while (antal_V >= 2) {
+			antal_X++;
+			antal_V -= 2;
+		}
+
+		for (int i = 0; i < antal_X; i++) {
+			nyString += "X";
+
+		}
+
+		for (int i = 0; i < antal_V; i++) {
 			nyString += "V";
 		}
-		
-		
-		for	(int i = 0; i < antal_I; i++) {
-			nyString += "I"; 
+
+		for (int i = 0; i < antal_I; i++) {
+			nyString += "I";
 		}
-		
-		
-		
+
 		return nyString;
 	}
 
