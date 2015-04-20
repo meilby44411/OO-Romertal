ecd21baeb11ccee18f20742114fc46887bee9e48
Lasse <Lasse@192.168.0.100>
2015-04-20 22:29:48 +0200

    test implementeret

--- OO-Romertal/src/src/Romertal.java
+++ OO-Romertal/src/src/Romertal.java
@@ -12,15 +12,17 @@ public class Romertal {
 		int antal_V = 0;
 		int antal_X = 0;
 		int antal_L = 0;
+		int antal_C = 0;
 
 		for (char c : a1) {
 			if (c == 'I')
 				antal_I++;
-			else if(c == 'V')
+			else if (c == 'V')
 				antal_V++;
-
-			else if ( c == 'X')
+			else if (c == 'X')
 				antal_X++;
+			else if (c == 'L')
+				antal_L++;
 		}
 
 		while (antal_I >= 5) {
@@ -32,16 +34,25 @@ public class Romertal {
 			antal_X++;
 			antal_V -= 2;
 		}
-		
-		while (antal_X >= 5){
+
+		while (antal_X >= 5) {
 			antal_L++;
-			antal_X-=5;
+			antal_X -= 5;
+		}
+
+		while (antal_L >= 2) {
+			antal_C++;
+			antal_L -= 2;
 		}
 		
+		for (int i = 0; i < antal_C; i++) {
+			nyString += "C";
+		}
+
+
 		for (int i = 0; i < antal_L; i++) {
 			nyString += "L";
-			}
-
+		}
 
 		for (int i = 0; i < antal_X; i++) {
 			nyString += "X";
