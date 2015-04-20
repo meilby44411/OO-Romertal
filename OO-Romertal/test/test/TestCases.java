8c3f286cff7aa5c8fd62894df85f2fbc181fc2db
Lasse <Lasse@192.168.0.100>
2015-04-20 20:42:28 +0200

    sat project op, lave metodekrop til første test

---
+++ OO-Romertal/test/test/TestCases.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class TestCases {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testIplusIIequalsIII() {
+	}
+
+}
